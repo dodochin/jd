2eb9fe3ae3e02c92c185a877eee69aac302a21e0
wishcaiji <henanmm521@gmail.com>
2021-03-31 14:49:27 +0800

    Update jd_plantBean.js

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -35,9 +35,9 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
                    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'savlhrg5x3lus4ocduqavx6ore@savlhrg5x3lus4ocduqavx6ore@7imgugr2v6clcu26ezhlngnxwli2wbzistqc33i@7qol36k2wexakflk6e6rmdjwrfz2g2rlfzgdazq',
+  'eeexxudqtlamp2rbf6dbne36kzmfxv2ktlci7hi@7d4a4cnl77cugdpbayoo5jt3ba@t7obxmpebrxkdygddkbhqpe4kymzu5lnvdxfdkq',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'savlhrg5x3lus4ocduqavx6ore@savlhrg5x3lus4ocduqavx6ore@7imgugr2v6clcu26ezhlngnxwli2wbzistqc33i@7qol36k2wexakflk6e6rmdjwrfz2g2rlfzgdazq',
+  'eeexxudqtlamp2rbf6dbne36kzmfxv2ktlci7hi@7d4a4cnl77cugdpbayoo5jt3ba@t7obxmpebrxkdygddkbhqpe4kymzu5lnvdxfdkq',
 ]
 let allMessage = ``;
 let currentRoundId = null;//本期活动id
