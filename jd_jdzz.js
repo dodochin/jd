d1abc5ccaadf6c50a100e255fab1736306f88f56
wishcaiji <henanmm521@gmail.com>
2021-03-31 15:08:40 +0800

    Update jd_jdzz.js

--- jd_jdzz.js
+++ jd_jdzz.js
@@ -44,8 +44,8 @@ let nowTimes = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 
 !(async () => {
   $.tuanList = []
   await requireConfig();
-  //if (helpAuthor) await getAuthorShareCode('https://raw.githubusercontent.com/l499477004/updateTeam/raw/main/jd_zz.json');
-  if (helpAuthor) await getAuthorShareCode('https://raw.githubusercontent.com/l499477004/updateTeam/raw/main/jd_zz.json');
+  //if (helpAuthor) await getAuthorShareCode('https://raw.githubusercontent.com/l499477004/updateTeam/master/jd_zz.json');
+  if (helpAuthor) await getAuthorShareCode('https://raw.githubusercontent.com/l499477004/updateTeam/master/jd_zz.json');
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
